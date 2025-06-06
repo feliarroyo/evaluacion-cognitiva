fc53b892eb28a893e57b39cc7e8501b48d80a704
feliarroyo <farroyo@alumnos.exa.unicen.edu.ar>
2025-06-06 13:49:17 -0300

    Primer commit en partes

---
+++ Assets/SpinImage.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class SpinImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, 1f * Time.deltaTime);
+    }
+}
